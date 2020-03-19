e2fdc693c4499325c0c46c97f0c6fed731dc7924
Hintee <dan.hintea@ebriza.com>
2020-03-19 17:12:31 +0200

    Impement

---
+++ Src/CmdWebApp/CmdWebApp/Controllers/CmdController.cs
@@ -0,0 +1,52 @@
+﻿using CmdWebApp.Models;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Web.Http;
+
+namespace CmdWebApp.Controllers
+{
+    public class CmdController : ApiController
+    {
+        // POST: api/Cmd
+        public IHttpActionResult Post(Cmd cmd)
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = cmd.Path,
+                    Arguments = cmd.Args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            StringBuilder printer = new StringBuilder();
+
+            DateTime startedAt = DateTime.Now;
+
+            try
+            {
+                proc.Start();
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    if ((DateTime.Now - startedAt).TotalSeconds > cmd.ResponseWaitTimeoutInSeconds)
+                    {
+                        proc.Kill();
+                        return Ok("Timed out");
+                    }
+
+                    printer.AppendLine(proc.StandardOutput.ReadLine());
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+            return Ok(printer.ToString());
+        }
+    }
+}
---
+++ Src/CmdWebApp/CmdWebApp/Models/Cmd.cs
@@ -0,0 +1,9 @@
+﻿namespace CmdWebApp.Models
+{
+    public class Cmd
+    {
+        public string Path { get; set; }
+        public string Args { get; set; }
+        public int ResponseWaitTimeoutInSeconds { get; set; } = 30;
+    }
+}
