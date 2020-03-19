752d87d481d8a8ba08da3e1d154b5a09cd2ef6ea
Hintee <dan.hintea@ebriza.com>
2020-03-19 16:44:27 +0200

    Scaffold solution

---
+++ Src/CmdWebApp/CmdWebApp/Controllers/HealthController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CmdWebApp.Controllers
+{
+    public class HealthController : ApiController
+    {
+        // GET: api/Health
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Health/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Health
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Health/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Health/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Src/CmdWebApp/CmdWebApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CmdWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Content("CMD via HTTP");
+        }
+    }
+}
